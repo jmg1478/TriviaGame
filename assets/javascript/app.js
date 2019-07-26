aeee07c8bab0cb70725717847bd142b18afe0452
Johnathan Mundt <johnathanmg1478@gmail.com>
2019-07-25 19:20:02 -0700

    JS planning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,36 @@
-// creat a funtion
+// creat a funtion that hold questions
 
+// funtion for actual quiz
+
+// store HTML output    
+
+// var for answers
+
+// forloop for possible answers
+
+// button user can click
+
+// way to link Questions to answers
+
+// string java to HTML
+
+
+// gather information
+
+// keep track of correct answered
+
+// find user input
+
+// if statement for correct 
+
+// else statment if wrong
+
+// at the end show the correct amount
+
+// slide function
+
+// display on start up
+
+// click funtion for going through quiz
+
+// timer for quiz
